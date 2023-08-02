7707c09f8a9851f7d14496c6aca69a678c4a9f42
Todd Garrison <toddlgarrison@gmail.com>
2023-08-02 16:59:08 -0400

    Add MatchReader.ts

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,13 +1,14 @@
 import fs from 'fs';
-import { dateStringToDate } from './Utils';
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
+export abstract class CsvFileReader {
     data: MatchData[] = [];
 
     constructor(public filename: string) {}
+    
+    abstract mapRow(row: string[]): MatchData;
 
     read():void {
         this.data = fs.readFileSync(this.filename, {
@@ -20,17 +21,5 @@ export class CsvFileReader {
                 }
             )
             .map(this.mapRow);
-    }
-
-    mapRow(row: string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-        ]
     }
 }
