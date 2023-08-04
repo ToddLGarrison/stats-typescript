58bd97d6937ae90b1703f9dd8dbdcbb7e0cebd9b
Todd Garrison <toddlgarrison@gmail.com>
2023-08-04 12:40:04 -0400

    Update MatchReader

--- build/MatchReader.js
+++ build/MatchReader.js
@@ -1,20 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Utils_1 = require("./Utils");
 class MatchReader {
     constructor(reader) {
         this.reader = reader;
+        this.matches = [];
     }
     load() {
+        this.reader.read();
+        this.reader.data.map((row) => {
+            return [
+                (0, Utils_1.dateStringToDate)(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                row[5],
+                row[6]
+            ];
+        });
     }
 }
-// .map((row: string[]): MatchData => {
-//     return [
-//         dateStringToDate(row[0]),
-//         row[1],
-//         row[2],
-//         parseInt(row[3]),
-//         parseInt(row[4]),
-//         row[5] as MatchResult,
-//         row[6]
-//     ]
-// })
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -9,23 +9,24 @@ interface DataReader {
 }
 
 class MatchReader {
+    matches: MatchData[] = []
     constructor(public reader: DataReader) {}
 
     load(): void{
-        
+        this.reader.read();
+        this.reader.data.map((row: string[]): MatchData => {
+            return [
+                dateStringToDate(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                row[5] as MatchResult,
+                row[6]
+            ]
+        })
     }
 }
 
 
 
-// .map((row: string[]): MatchData => {
-//     return [
-//         dateStringToDate(row[0]),
-//         row[1],
-//         row[2],
-//         parseInt(row[3]),
-//         parseInt(row[4]),
-//         row[5] as MatchResult,
-//         row[6]
-//     ]
-// })
