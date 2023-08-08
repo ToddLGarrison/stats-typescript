5035bcd9a1b42f6bb9e2f7af0547cb476248f3f7
Todd Garrison <toddlgarrison@gmail.com>
2023-08-08 13:59:19 -0400

    update Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
